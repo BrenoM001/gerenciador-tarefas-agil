eaf1153a35b749c8e0cef096eb1b424b6b9f2dc4
Breno Martins <brenomartins307@gmail.com>
2025-07-29 21:34:29 -0300

    test: adiciona testes para função criar_tarefa

--- tests/test_tarefas.py
+++ tests/test_tarefas.py
@@ -1,2 +1,9 @@
-def test_valida_soma():
-    assert 3 + 4 == 7
+from SRC.tarefas import criar_tarefa
+
+def test_criar_tarefa_com_nome_valido():
+    resultado = criar_tarefa("Estudar algoritmos")
+    assert resultado == "Tarefa 'Estudar algoritmos' criada com sucesso"
+
+def test_criar_tarefa_com_nome_vazio():
+    resultado = criar_tarefa("")
+    assert resultado == "Nome da tarefa inválido"
